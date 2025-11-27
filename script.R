394f29d920f26924b156b68388e13583e64bf6e9
NicoSteffen <nico@die-steffens.de>
2025-11-27 11:10:29 +0100

    Update

---
+++ script.R
@@ -0,0 +1,24 @@
+# https://stephangoerigk.github.io/CFH_R_bookdown/über-dieses-skript.html
+# https://github.com/NicoSteffen/Fit-fuer-R_Statistische_Modellierung_Visualisierung
+
+
+
+
+
+
+
+Datenvisualisierung mit ggplot2:
+  Kennenlernen der Prinzipien von ggplot2 zur ansprechenden und informativen Visualisierung komplexer Datenbeziehungen.
+Erstellen verschiedener Diagramme mit zwei oder drei Variablen (Streudiagramm, Histogramm, Balkendiagramm, Boxplots).
+Facetting und Ästhetische Anpassungen.
+Regressionsanalysen:
+  Berechnung von einfachen und multiplen Regressionsmodellen.
+Interpretation der Ergebnisse und der Modell-Outputs.
+Überprüfung der statistischen Voraussetzungen.
+Durchführung von Post-Hoc-Trendanalysen zur tiefergehenden Untersuchung spezifischer Effekte.
+Varianzanalysen (ANOVA):
+  Einführung in verschiedene ANOVA-Designs: einfaktorielle und mehrfaktorielle ANOVA.
+Behandlung komplexer Designs wie Repeated Measures und Mixed Designs.
+Überprüfung der Voraussetzungen für ANOVA.
+Interpretation der Ergebnisse und die Durchführung von Post-Hoc-Tests zur genaueren Lokalisierung signifikanter Unterschiede.
+Melde dich an, wenn du (nochmal) lernen willst, wie du professionelle statistische Analysen und Visualisierungen mit R durchführst!
